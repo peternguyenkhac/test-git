b88eb54f87d635f8454de8353e185ab862e031f8
HaiNK <hainguyenkhac01@gmail.com>
2024-11-05 09:36:03 +0700

    merge test hello world update

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 function abc (){
     var a = 123;
     console.log("hello world");
-<<<<<<< HEAD
-    console.log("updated 123");
-=======
     a += 1;
     console.log(a);
->>>>>>> 7aebc9225467eaf6e92bd184337388f13a4db23a
 }
